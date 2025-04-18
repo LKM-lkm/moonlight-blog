905fa5912f36a77ac94cf343c0fad95aa7b0c4b4
LKM-lkm <lkm836972@outlook.com>
2025-04-18 21:03:57 +0800

    添加聊天机器人功能

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// 月光云海博客 - 主入口文件
+console.log('月光云海博客已加载');
+
+// 导入样式
+import '../assets/css/admin/login.css';
+
+// 这里可以添加其他导入和初始化代码 
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '/'),
+    },
+    compress: true,
+    port: 9000,
+  },
+}; 
