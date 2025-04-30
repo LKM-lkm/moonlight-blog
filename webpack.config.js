e1f4176387f511164e9c9780078f6ac16f22f760
LKM-lkm <lkm836972@outlook.com>
2025-04-30 21:20:44 +0800

    fix: 修复部署配置，更新构建输出路径

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'js/[name].[contenthash].js',
     clean: true,
-    publicPath: '/'
+    publicPath: './'
   },
   module: {
     rules: [
